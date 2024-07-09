9014ef219df827112339e7f226e7280be697d473
ZIPcde <votmoyyashik100@gmail.com>
2024-07-09 23:37:35 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductDetails />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from './components/ProductDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductDetails
   }
 }
 </script>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>{{ product.name }}</h2>
+      <p>{{ formattedPrice }}</p>
+      <p v-if="product.available">Available</p>
+      <p v-else>Out of stock</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        product: {
+          name: 'Молоток строительный',
+          price: 99.99,
+          available: false,
+        }
+      };
+    },
+    computed: {
+      formattedPrice() {
+        return '$' + this.product.price.toFixed(2);
+      }
+    }
+  };
+  </script>
